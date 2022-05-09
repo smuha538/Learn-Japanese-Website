d6014c65b5056f7c6dcf307005023483da7f1797
Sahir <sahirbaloch10@gmail.com>
2022-05-09 01:42:53 -0600

    minor changes

--- partials/database.php
+++ partials/database.php
@@ -2,6 +2,7 @@
 require "../vendor/autoload.php";
 
 $server = $_ENV['MONGODB_URI'] ?? '';
+// $server = "mongodb+srv://databaseConnector:wSb7XnA0keBnIhuc@userdatacluster.adumz.mongodb.net/Data?retryWrites=true";
 
 $client = new MongoDB\Client($server);
 $data = $client->Data;
