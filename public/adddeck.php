6a3178dd8f8cc827aa028594660d471145c141ad
Sahir <sahirbaloch10@gmail.com>
2022-05-11 20:34:54 -0600

    minor changes

--- public/adddeck.php
+++ public/adddeck.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-header('Content-Type: application/json');
 
 if (isset($_GET["deck"])) {
 
--- public/loaddeck.php
+++ public/loaddeck.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-header('Content-Type: application/json');
 
 if (isset($_SESSION["decks"]) && !empty($_SESSION["decks"])) {
     $deck = $_SESSION["decks"];
